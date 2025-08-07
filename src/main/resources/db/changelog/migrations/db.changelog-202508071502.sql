1319c44dcca3bd410f89e338527d05173bd37cca
ever-johsue-ruiz-serrano <27361611everjohsue@gmail.com>
2025-08-07 21:25:23 +0000

    projeto board

---
+++ src/main/resources/db/changelog/migrations/db.changelog-202508071502.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+--changeset ever:202508071502
+--comment: boards table create
+
+CREATE TABLE BOARDS(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+) ENGINE=InnoDB;
+
+--rollback DROP TABLE BOARDS
